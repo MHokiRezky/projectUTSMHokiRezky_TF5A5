f470099a1353ec475ff83468f17dbf138b075eee
MHokiRezky <m.hoki.rezky@gmail.com>
2022-11-01 21:49:23 +0700

    projectUTSMHokiRezky_TF5A5
    Mohammad Hoki Rezky_202010225110

---
+++ UTSSoal1_MHokiRezky.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+main() {
+  print ("------------------------ Proses Input Data ------------------------");
+
+  stdout.write("Nama           : ");
+  var nama = stdin.readLineSync();
+
+ stdout.write("Kelas           : ");
+  var kelas = stdin.readLineSync();
+
+stdout.write("Nilai UTS        : ");
+  var nilai = stdin.readLineSync();
+
+stdout.write("Mata kuliah      : ");
+  var matkul = stdin.readLineSync();
+
+  
+}
+
